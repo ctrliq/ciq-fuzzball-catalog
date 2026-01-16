2840cb1b6390e98745f24b1256db8d3c3228161e
Wolfgang Resch <wresch@ciq.com>
2026-01-16 10:16:21 -0500

    Fixing template and scripts for experiment 1 to be more robust

--- applications/lammps_gpu/experiments/level1/lenard-jones-fluid/my-first-input/stageout.sh
+++ applications/lammps_gpu/experiments/level1/lenard-jones-fluid/my-first-input/stageout.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 set -ex
 
-run="${1}"
+run_dir="${1}"
 dest="${2}"
 
+run="$(basename $run_dir)"
+
 mkdir -p "${dest}"
-if [ -d  "${run}" ] ; then
-    tar -czf "${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz" "${run}"
+if [ -d  "${run_dir}" ] ; then
+    tar -czf "${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz" "${run_dir}"
     echo "saved results to ${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz"
 else
     echo "did not save results to persistent store"
