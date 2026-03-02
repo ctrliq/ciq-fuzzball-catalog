ddaf5f33922f930d12c38ff058d2cb5e5fe85edb
Wolfgang Resch <wresch@ciq.com>
2026-03-02 15:37:27 -0500

    Pull from more recent commit of example repo

--- applications/lammps_gpu/experiments/level1/lennard-jones-fluid/improved-input/stagein.sh
+++ applications/lammps_gpu/experiments/level1/lennard-jones-fluid/improved-input/stagein.sh
@@ -5,7 +5,7 @@ set -e
 run_dir="${1}"
 mkdir -p "${run_dir}" && cd "${run_dir}"
 
-commit="4e249a6f7a0f8f8057bf72c07d39841a297e69a6"
+commit="dd3dce70b883af02df293ce52491beabd8e01928"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
 
 wget -q -O "input.lammps" "${url}/level1/lennard-jones-fluid/improved-input/input.md.lammps"
