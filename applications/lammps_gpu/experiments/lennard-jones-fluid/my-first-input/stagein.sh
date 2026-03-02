edc52d0278c2455b4c2ede3e69fe77abeb72d1c2
Wolfgang Resch <wresch@ciq.com>
2026-03-02 15:55:52 -0500

    Adapting to new tutorial data repo layout

--- applications/lammps_gpu/experiments/lennard-jones-fluid/my-first-input/stagein.sh
+++ applications/lammps_gpu/experiments/lennard-jones-fluid/my-first-input/stagein.sh
@@ -7,4 +7,4 @@ mkdir -p "${run_dir}" && cd "${run_dir}"
 
 commit="dd3dce70b883af02df293ce52491beabd8e01928"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
-wget -q -O input.lammps "${url}/level1/lennard-jones-fluid/my-first-input/input.lammps"
+wget -q -O input.lammps "${url}/level1/lennard-jones-fluid/my-first-input/initial.lmp"
