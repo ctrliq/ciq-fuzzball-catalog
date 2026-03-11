aed50b37255e8eda4834d5893d0606c095c06fbb
Wolfgang Resch <wresch@ciq.com>
2026-03-11 10:28:43 -0400

    Fixes lint

--- applications/lammps_gpu/experiments/lennard-jones-fluid/my-first-input/lammps.sh
+++ applications/lammps_gpu/experiments/lennard-jones-fluid/my-first-input/lammps.sh
@@ -27,5 +27,4 @@ export PATH LD_LIBRARY_PATH LAMMPS_POTENTIALS MSI2LMP_LIBRARY
 cd "${run_dir}" || exit 100
 [[ -e "${prefix}/bin/lmp" ]] || { printf "Unable to find lammps executable for GPU architecture\n"; exit 101; }
 lmp -k on g "${gpus_per_node}" -sf kk -pk kokkos cuda/aware on \
-    neigh full comm device binsize 2.8 -var x 8 -var y 4 -var z 8 \
-    -in input.lammps
+    neigh full comm device binsize 2.8 -in input.lammps
